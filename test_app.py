0c338cf0b2dd5e1e1143416cba1f8fb6e2605587
Daniel Pape <>
2024-05-19 11:19:35 +0200

    test: Add test -2 + 0 = -2

--- test_app.py
+++ test_app.py
@@ -10,6 +10,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(-1, 0), -1)
         self.assertEqual(add(1, 0), 1)
         self.assertEqual(add(0, 0), 0)
+        self.assertEqual(add(-2, 0), -2)
 
 
 if __name__ == "__main__":
