40ded78b90a09196940b1b7361a4caeeaf8efc53
Daniel Pape <>
2024-05-19 09:45:26 +0200

    Add new test

--- test_app.py
+++ test_app.py
@@ -7,6 +7,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add(3, 5), 8)
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(-1, -1), -2)
+        self.assertEqual(add(-1, 0), 0)
 
 
 if __name__ == "__main__":
